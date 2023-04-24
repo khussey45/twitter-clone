c3ba8827595e6096b9a7edff1266d4b733c79a91
khussey45 <khussey45@gmail.com>
2023-04-24 18:53:29 -0400

    loadPosts

--- components/posts/PostFeed.tsx
+++ components/posts/PostFeed.tsx
@@ -1,6 +1,23 @@
-const PostFeed = () => {
+import usePosts from "@/hooks/usePosts";
+
+import PostItem from "./PostItem";
+
+interface PostFeedProps {
+  userId?: string;
+}
+
+const PostFeed: React.FC<PostFeedProps> = ({ userId }) => {
+  const { data: posts = [] } = usePosts(userId);
   return (
-    <div></div>
+    <>
+      {posts.map((post: Record<string, any>) => (
+        <PostItem 
+          userId={userId}
+          key={post.id}
+          data={post}
+        /> 
+      ))}
+    </>
   );
 }
 
